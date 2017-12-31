50e867eb8afc045820feab9ec58242c2c571d1d5
yabeow <yabeow@protonmail.com>
2017-12-31 13:48:50 +0700

    [EsLint] ! Fix warning in config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   extends: 'airbnb',
   rules: {
-    'no-use-before-define': ['error', { functions: false, classes: false, variables: false }],
+    'no-use-before-define': ['error', { functions: 0, classes: 0, variables: 0 }],
     'react/jsx-filename-extension': [
       1,
       {
         extensions: ['.js', '.jsx'],
       },
     ],
-    'react/prefer-stateless-function': false,
-    'react/forbid-prop-types': false,
+    'react/prefer-stateless-function': 0,
+    'react/forbid-prop-types': 0,
   },
 };
